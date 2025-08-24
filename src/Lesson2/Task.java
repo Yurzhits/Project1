d06990b8b5e6f043627ac8b614116f8acab08459
Yurzhits <m_yurzhits@mail.ru>
2025-08-25 01:05:54 +0300

    Новый класс

---
+++ src/Lesson2/Task.java
@@ -0,0 +1,10 @@
+package Lesson2;
+
+public class Task {
+    public static void main(String[] args) {
+        System.out.println("Счет до 23");
+        for (int i = 0; i < 24; i++) {
+            System.out.println(i);
+        }
+    }
+}
