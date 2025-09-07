2429bb0a8fc74ad50163bed1580b0c3bf65bf794
Yurzhits <m_yurzhits@mail.ru>
2025-09-07 15:21:01 +0300

    Task1 поля и методы

--- src/Lesson6/Task1.java
+++ src/Lesson6/Task1.java
@@ -10,15 +10,18 @@ package Lesson6;
 
 public class Task1 {
     public static void main(String[] args) {
+
         CreditCard creditCard1 = new CreditCard(123456, 450);
         CreditCard creditCard2 = new CreditCard(654321, 5550);
         CreditCard creditCard3 = new CreditCard(111111, 675);
+
         System.out.println("Первая карта:");
         double sum1 = creditCard1.plusCash();
         System.out.println("Вторая карта:");
         double sum2 = creditCard2.plusCash();
         System.out.println("Третья карта:");
         double sum3 = creditCard3.minusCash();
+
         creditCard1.info();
         creditCard2.info();
         creditCard3.info();
