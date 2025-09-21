c2dc2a2230a212eb6126fd0e86dd5dbb8a807a10
Yurzhits <m_yurzhits@mail.ru>
2025-09-21 22:59:05 +0300

    Регулярные выражения

--- src/Lesson12/Task1.java
+++ src/Lesson12/Task1.java
@@ -1,4 +1,21 @@
 package Lesson12;
 
+//Вывести в консоль из строки которую пользователь вводит с клавиатуры все аббревиатуры.
+//Аббревиатурой будем считать слово от 2 до 6 символов, состоящее только из прописных букв, без чисел.
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку с аббревиатурами:");
+        String str = scanner.nextLine();
+        Pattern abbreviation = Pattern.compile("[А-Я]{2,6}|[A-Z]{2,6}");
+        Matcher m = abbreviation.matcher(str);
+        while (m.find()) {
+            System.out.println(m.group());
+        }
+    }
 }
--- src/Lesson12/Task2.java
+++ src/Lesson12/Task2.java
@@ -1,4 +1,41 @@
 package Lesson12;
 
+//Программа на вход получает произвольный текст.
+//В этом тексте может быть номер документа(один или несколько), емейл и номер телефона.
+//Номер документа в формате: xxxx-xxxx-xx, где x - это любая цифра;
+//номер телефона в формате: +(xx)xxxxxxx.
+//Документ может содержать не всю информацию, т.е. например, может не содержать номер телефона, или другое.
+//Необходимо найти эту информацию и вывести в консоль в формате:
+//email: teachmeskills@gmail.com
+//document number: 1423-1512-51
+//и т.д
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст:");
+        String text = scanner.nextLine();
+        Pattern numDoc = Pattern.compile("\\d{4}-\\d{4}-\\d{2}");
+        Pattern tel = Pattern.compile("\\+?\\(\\d{2}\\)\\d{7}");
+        Pattern email = Pattern.compile("[a-zA-Z]{2,}@[a-z]{2,}\\.[a-z]{2,}");
+        Matcher emailMat = email.matcher(text);
+        if (emailMat.find()) {
+            System.out.println("email: " + emailMat.group());
+        }
+        Matcher numDocMat = numDoc.matcher(text);
+        while (numDocMat.find()) {
+            System.out.println("document number: " + numDocMat.group());
+        }
+        Matcher telMat = tel.matcher(text);
+        if (telMat.find()) {
+            System.out.println("telephone number: " + telMat.group());
+        }
+    }
 }
+
+//Текст для проверки:
+//Привет, мой email - teachmeskills@gmail.com, номер документа 1423-1512-51 и номер телефона +(12)3456789.
